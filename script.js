ef5c4234ff8b686111346dd86c78a479406e215b
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-21 17:41:28 +0100

    create updateProgress

--- script.js
+++ script.js
@@ -29,9 +29,12 @@ const togglePlay = () => {
 // On Video End, Show play button icon
 video.addEventListener('ended', showPlayIcon);
 
-
 // Progress Bar ---------------------------------- //
 
+// Update progress bar as video plays
+const updateProgress = () => {
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+}
 
 
 // Volume Controls --------------------------- //
@@ -47,3 +50,5 @@ video.addEventListener('ended', showPlayIcon);
 // Event Listeners
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
