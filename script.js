ede0c209c392d716d26d946cf853abbaf28992c2
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-21 18:17:43 +0100

    create changeVolume function

--- script.js
+++ script.js
@@ -55,6 +55,31 @@ const setProgress = e => {
 
 // Volume Controls --------------------------- //
 
+// Volume Bar
+const changeVolume = e => {
+    let volume = e.offsetX / volumeRange.offsetWidth;
+    // Rounding volume up or down
+    if (volume < 0.1) {
+        volume = 0;
+    }
+
+    if (volume > 0.9) {
+        volume = 1;
+    }
+
+    volumeBar.style.width = `${volume * 100}%`;
+    video.volume = volume;
+
+    // Change icon depending on volume
+    volumeIcon.className = '';
+    if (volume >= 0.7) {
+        volumeIcon.classList.add('fas', 'fa-volume-up');
+    } else if (volume < 0.7 && volume > 0) {
+        volumeIcon.classList.add('fas', 'fa-volume-down');
+    } else if (volume === 0) {
+        volumeIcon.classList.add('fas', 'fa-volume-off');
+    }
+}
 
 
 // Change Playback Speed -------------------- //
@@ -69,3 +94,4 @@ video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
+volumeRange.addEventListener('click', changeVolume);
