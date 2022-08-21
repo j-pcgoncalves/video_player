c147100067540a1c660670c04891f4ca6ea60115
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-21 18:34:31 +0100

    create changeSpeed function

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const volumeRange = document.querySelector('.volume-range');
 const volumeBar = document.querySelector('.volume-bar');
 const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
+const speed = document.querySelector('.player-speed');
 const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
@@ -104,8 +105,9 @@ const toggleMute = () => {
 }
 
 // Change Playback Speed -------------------- //
-
-
+const changeSpeed = () => {
+    video.playbackRate = speed.value;
+}
 
 // Fullscreen ------------------------------- //
 
@@ -117,3 +119,4 @@ video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
 volumeIcon.addEventListener('click', toggleMute);
+speed.addEventListener('change', changeSpeed);
