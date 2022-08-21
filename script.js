178f3be29200116b206b9a778132c5d0793cbe6b
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-21 18:02:44 +0100

    create setProgress

--- script.js
+++ script.js
@@ -46,6 +46,12 @@ const updateProgress = () => {
     duration.textContent = `${displayTime(video.duration)}`;
 }
 
+// Click to seek within the video
+const setProgress = e => {
+    const newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime * 100}%`;
+    video.currentTime = newTime * video.duration;
+}
 
 // Volume Controls --------------------------- //
 
@@ -62,3 +68,4 @@ playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
