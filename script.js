d600dd8e821bb1197e9a248fdf69382272ac5667
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-21 17:33:24 +0100

    creating togglePlay and showPlayIcon

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
+const showPlayIcon = () => {
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+}
 
+const togglePlay = () => {
+    if (video.paused) {
+        video.play();
+        playBtn.classList.replace('fa-play', 'fa-pause');
+        playBtn.setAttribute('title', 'Pause');
+    } else {
+        video.pause();
+        showPlayIcon();
+    }
+}
+
+// On Video End, Show play button icon
+video.addEventListener('ended', showPlayIcon);
 
 
 // Progress Bar ---------------------------------- //
@@ -18,4 +44,6 @@
 
 // Fullscreen ------------------------------- //
 
-
+// Event Listeners
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
